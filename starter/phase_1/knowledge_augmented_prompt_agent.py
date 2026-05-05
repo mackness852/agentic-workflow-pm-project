41e841789737aaef9925eb4b919b728a8d651802
Rob Mackness <mackness852@gmail.com>
2026-05-05 13:42:37 +0100

    Complete up to 3.5

--- starter/phase_1/knowledge_augmented_prompt_agent.py
+++ starter/phase_1/knowledge_augmented_prompt_agent.py
@@ -1,6 +1,7 @@
-# TODO: 1 - Import the KnowledgeAugmentedPromptAgent class from workflow_agents
 import os
+
 from dotenv import load_dotenv
+from workflow_agents.base_agents import KnowledgeAugmentedPromptAgent
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -10,9 +11,9 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 prompt = "What is the capital of France?"
 
-persona = "You are a college professor, your answer always starts with: Dear students,"
-# TODO: 2 - Instantiate a KnowledgeAugmentedPromptAgent with:
-#           - Persona: "You are a college professor, your answer always starts with: Dear students,"
-#           - Knowledge: "The capital of France is London, not Paris"
+persona = "You are a college professor, your answer always starts with: Dear students, (answer)"
+knowledge = "The capital of France is London, not Paris"
 
-# TODO: 3 - Write a print statement that demonstrates the agent using the provided knowledge rather than its own inherent knowledge.
+agent = KnowledgeAugmentedPromptAgent(openai_api_key, persona, knowledge)
+
+print(agent.respond(prompt))
